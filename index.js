feae4d9f2893dd503762e264ceda741d0428ac85
Kolyan1944 <119818259+Kolyan1944@users.noreply.github.com>
2023-12-14 20:33:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const magicBall = document.getElementById('magicBall');
+const resultDiv = document.getElementById('result');
+const circleImage = document.getElementById('circleImage');
+
+magicBall.addEventListener('click', () => {
+    const responses = [
+        'Так',
+        'Ні',
+        'Можливо',
+        'Не впевнений, але так',
+        'Не впевнений, але ні'
+    ];
+
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    const randomResponse = responses[randomIndex];
+
+    resultDiv.textContent = randomResponse;
+
+    // Додайте анімацію для магічної кулі
+    magicBall.style.transform = 'scale(1.2)';
+    setTimeout(() => {
+        magicBall.style.transform = 'scale(1)';
+    }, 300);
+});
